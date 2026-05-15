08e6402ea4662b40d9c46b6a371e80ad40aa535e
Yhuan Shin <yhuanft@stpeter.com.ph>
2026-05-15 16:44:04 +0800

    feat: implement local JSON data fetching for plans and sections, add fs module

--- app/api/products/plan.ts
+++ app/api/products/plan.ts
@@ -1,4 +1,6 @@
+import path from "path";
 import { url } from "../";
+import fs from "fs";
 
 
 export async function getModeAndName(planDesc: string, selectedPlan: string) {
@@ -17,17 +19,26 @@ export async function getProductByName(planDesc: string) {
 
 export async function getPlansCard() {
     const api = `${url}api/Product/GetPlanCards`;
-    const res = await fetch(api);
-    const data = await res.json();
+    // const res = await fetch(api);
+    // const data = await res.json();
+    const filePath = path.join(process.cwd(), "data/plan_dummy.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
     return Array.isArray(data) ? data : [data];
 }
 
 export async function getPlansSection() {
     const api = `${url}api/Product/GetPlanSections`;
-    console.log("API URL for getPlansSection:", api); // Debug log to check the API URL
-    const res = await fetch(api);
-    console.log("Response from getPlansSection:", res); // Debug log to check the response
-    const data = await res.json();
+    
+    // console.log("API URL for getPlansSection:", api); // Debug log to check the API URL
+    // const res = await fetch(api);
+    // console.log("Response from getPlansSection:", res); // Debug log to check the response
+    // const data = await res.json();
+    const filePath = path.join(process.cwd(), "data/plansection_dummy.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+
+    const data = JSON.parse(jsonData);
+
     return Array.isArray(data) ? data : [data];
 }
 
