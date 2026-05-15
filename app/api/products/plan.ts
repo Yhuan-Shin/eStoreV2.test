be3dad865dabbb6396a6f7ea7579723ecfa4ffdb
Yhuan Shin <yhuanft@stpeter.com.ph>
2026-05-15 16:57:41 +0800

    feat: add local JSON data files for product plans and update data fetching methods

--- app/api/products/plan.ts
+++ app/api/products/plan.ts
@@ -5,15 +5,21 @@ import fs from "node:fs";
 
 export async function getModeAndName(planDesc: string, selectedPlan: string) {
   const api = `${url}/GetModeAndName?planDesc=${planDesc}&mode=${selectedPlan}`;
-  const res = await fetch(api);
-  const data = await res.json();
+  // const res = await fetch(api);
+  // const data = await res.json();
+    const filePath = path.join(process.cwd(), "data/ProductByModeName_dummy.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
   return data;
 }
 
 export async function getProductByName(planDesc: string) {
   const api = `${url}/GetProductByName?planDesc=${planDesc}`;
-  const res = await fetch(api);
-  const data = await res.json();
+  // const res = await fetch(api);
+  // const data = await res.json();
+    const filePath = path.join(process.cwd(), "data/ProductByName_dummy.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
   return Array.isArray(data) ? data : [data];
 }
 
@@ -29,7 +35,6 @@ export async function getPlansCard() {
 
 export async function getPlansSection() {
     const api = `${url}api/Product/GetPlanSections`;
-    
     // console.log("API URL for getPlansSection:", api); // Debug log to check the API URL
     // const res = await fetch(api);
     // console.log("Response from getPlansSection:", res); // Debug log to check the response
